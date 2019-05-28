af313f4f81a32f327703e0ce9eab6acdd746e43c
Darryl <darrylmangibin@gmail.com>
2019-05-28 20:27:38 +0800

    boilerplate for app component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>TechStack</Text>
+      </View>
+    );
+  };
+};
+
+export default App;
